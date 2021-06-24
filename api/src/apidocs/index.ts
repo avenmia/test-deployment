c7ed370a70cc096f2eefe934e50d4d41010d6179
Michael Waddell <michael@waddellnet.com>
2021-06-24 13:33:50 -0500

    echo headers

--- api/src/apidocs/index.ts
+++ api/src/apidocs/index.ts
@@ -1,11 +1,10 @@
-import { AzureFunction, Context } from "@azure/functions";
-import * as apidoc from './apidoc.json';
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
-const httpTrigger: AzureFunction = async function (context: Context): Promise<void> 
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> 
 {
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: JSON.stringify(apidoc)
+        body: JSON.stringify(req.headers)
     };
 };
 
